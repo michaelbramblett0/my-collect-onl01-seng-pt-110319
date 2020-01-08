cbd6a9979ca1daeec25ca5c7262c1997ff1e2405
Michael Bramblett <michaelbramblett0@gmail.com>
2020-01-08 23:35:59 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,5 +1,16 @@
-list = ["bw", "qw", "aa"]
+# list = ["bw", "qw", "aa"]
+
+# def my_collect(array)
+#   while array
 
 def my_collect(array)
-  while array
-
+  i = 0
+  name_collection = []
+  while i < array.length
+    name_collection.push yield(array[i])
+  # you could also do it this way:
+  # name_collection << yield(array[i])
+    i += 1
+  end
+  name_collection
+end
